947d9adaa58d36c34a8bb9e097ddf247bc5af896
Neeraj Sharma <nsxylush@gmail.com>
2024-01-03 22:16:20 +0530

    set jvmTarget to 1.8
    There was a compatibility issue due to different jvm target.

--- kotlinutils/build.gradle.kts
+++ kotlinutils/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
     alias(libs.plugins.org.jetbrains.kotlin.jvm)
@@ -5,10 +7,13 @@ plugins {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
 }
 
-kotlin {
-//    jvmToolchain(1)
+
+tasks.withType(KotlinCompile::class.java).all {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
